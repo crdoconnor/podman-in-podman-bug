aa40944fc20cf5946141272b48fc4a998d77d893
Colm O'Connor <colm.oconnor.github@gmail.com>
2022-08-06 17:02:42 +0100

    Tweak

--- exhibitbug.sh
+++ exhibitbug.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 podman pull ubuntu:22.04
+podman image rm podman-bug || true
 podman volume rm podman-bug-container || true
 podman volume create podman-bug-container
 podman build . -t podman-bug -f Dockerfile
