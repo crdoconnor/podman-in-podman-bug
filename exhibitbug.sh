5154c50202e49628c3ba8b5eb713b2b7147adb33
Colm O'Connor <colm.oconnor.github@gmail.com>
2022-08-06 17:18:36 +0100

    Changed from localstack to swagger ui

--- exhibitbug.sh
+++ exhibitbug.sh
@@ -1,1 +1,1 @@
-podman run --privileged -it --rm -v podman-bug-container:/var/lib/containers/storage podman-bug  podman pull docker.io/localstack/localstack
+podman run --privileged -it --rm -v podman-bug-container:/var/lib/containers/storage podman-bug  podman pull docker.io/swaggerapi/swagger-ui
