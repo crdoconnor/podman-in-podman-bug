460e35f3545c8a20da4c1305d4cb4fef4e2168b0
Colm O'Connor <colm.oconnor.github@gmail.com>
2022-08-06 16:45:10 +0100

    Set up to exhibit bug

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:22.04
+
+ENV TZ=Europe/London
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+RUN apt-get update
+RUN apt-get upgrade -y
+RUN apt-get install curl podman -y
---
+++ exhibitbug.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+podman pull ubuntu:22.04
+podman volume rm podman-bug-container || true
+podman volume create podman-bug-container
+podman build . -t podman-bug -f Dockerfile
+podman run --privileged -it --rm -v podman-bug-container:/var/lib/containers/storage podman-bug "podman pull docker.io/localstack/localstack"
